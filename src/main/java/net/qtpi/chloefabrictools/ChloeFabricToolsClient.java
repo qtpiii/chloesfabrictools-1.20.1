966726abf6f58a0acc849d853fc533049998f7aa
itssmuug <lefacef@gmail.com>
2024-01-15 12:54:13 -0700

    Initial commit

---
+++ src/main/java/net/qtpi/chloefabrictools/ChloeFabricToolsClient.java
@@ -0,0 +1,10 @@
+package net.qtpi.chloefabrictools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChloeFabricToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
